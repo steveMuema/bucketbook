bf3aa2d29faab7408e5017b02f431b4df1ce23b1
hitchs3x <hitchs3x@gmail.com>
2017-07-21 00:49:55 +0300

    routes for signup and signin work properly. minor bug hime page

--- app/views.py
+++ app/views.py
@@ -1,33 +1,38 @@
 """" use this to configure the @app/route paths in the html files """
-from flask import render_template
-from app import APP
+from app import app
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 
-@APP.route('/')
+@app.route('/')
 def index():
     """ navigates to the root page index.html """
-    return render_template("index.html")
+    if not session.get('logged in'):
+        return render_template("signin.html")
+    return render_template("home.html")
 
 
-@APP.route('/signup')
+@app.route('/signup')
 def signup():
     """ navigates to the signup page signup.html """
     return render_template("signup.html")
 
 
-@APP.route('/signin')
-def signin():
+@app.route('/signin', methods=['GET', 'POST'])
+def do_login():
     """ navigates to the sign page signin.html """
-    return render_template("signin.html")
+    if request.form['email'] == 'hitchs3x@gmail.com' and request.form['username'] == 'hitch' and request.form['password']=='say123#':
+        session['loggedin']=True
+    else:flash('Authenticaion error, Try again')
+    return render_template("home.html")
 
 
-@APP.route('/home')
+@app.route('/home')
 def home():
     """ navigates to the home page home.html """
     return render_template("home.html")
 
 
-@APP.route('/home/activities')
+@app.route('/home/activities')
 def activities():
     """ navigates to the activites page activities.html """
     return render_template("activities.html")
